e76092e248f1763b8bf6db983f40a7392b4ab4ba
god <naughty7878@163.com>
2025-05-30 23:49:00 +0800

    框架升级 JDK17

--- framework-payment-provider/src/test/java/com/test/Generator.java
+++ framework-payment-provider/src/test/java/com/test/Generator.java
@@ -10,34 +10,38 @@ import java.util.Collections;
 public class Generator {
 
     public static void main(String[] args) {
-        FastAutoGenerator.create("jdbc:mysql://47.106.74.177:13306/test_springcloud?allowPublicKeyRetrieval=true&useSSL=false",
-                        "hd", "hd123456")
+        String url = "jdbc:mysql://192.168.1.170:3306/test_springcloud?allowPublicKeyRetrieval=true&useSSL=false";
+        String username = "root";
+        String password = "123456";
+        String outputDir = "C:\\Users\\H__D\\Desktop";
+        String xmlPath = "C:\\Users\\H__D\\Desktop";
+
+        FastAutoGenerator.create(url, username, password)
                 .globalConfig(builder -> {
-                    builder.author("hd") // 设置作者
-//                            .enableSwagger() // 开启 swagger 模式
-                            .fileOverride() // 覆盖已生成文件
-                            .outputDir("C:\\Users\\\\H__D\\Desktop"); // 指定输出目录
+                    builder.author("H__D")                  // 设置作者
+//                            .enableSwagger()              // 开启 swagger 模式
+                            .outputDir(outputDir);          // 指定输出目录
                 })
                 .packageConfig(builder -> {
-                    builder.parent("com") // 设置父包名
-                            .moduleName("test.payment.provider") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.xml, "C:\\Users\\H__D\\Desktop")); // 设置mapperXml生成路径
+                    builder.parent("com")                   // 设置父包名
+                            .moduleName("test.payment.provider")           // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, xmlPath)); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("payment") // 设置需要生成的表名
-//                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                    builder.addInclude("payment")        // 设置需要生成的表名
+//                            .addTablePrefix("t_", "c_");  // 设置过滤表前缀
 
-                            .entityBuilder()  // 实体策略配置
-//                            .enableColumnConstant()  // 开启生成字段常量    默认值:false， 生成：public static final String LAST_NAME = "last_name";
-//                            .enableChainModel() // 开启链式模型    默认值:false 生成：@Accessors(chain = true)
-//                            .enableFileOverride() // 覆盖已生成文件    默认值:false
-                            .enableLombok() // 开启 lombok 模型    默认值:false
+                            .entityBuilder()                // 实体策略配置
+//                            .enableColumnConstant()       // 开启生成字段常量    默认值:false， 生成：public static final String LAST_NAME = "last_name";
+//                            .enableChainModel()           // 开启链式模型    默认值:false 生成：@Accessors(chain = true)
+//                            .enableFileOverride()         // 覆盖已生成文件    默认值:false
+                            .enableLombok()                 // 开启 lombok 模型    默认值:false
 //                            .enableTableFieldAnnotation()    // 开启生成实体时生成字段注解    默认值:false, 生成：@TableField("last_name")
-                            .mapperBuilder() // mapper 策略配置
-                            .superClass(BaseMapper.class) // 开启 @Mapper 注解    默认值:false
-                            .enableMapperAnnotation() // 启用 BaseResultMap 生成    默认值:false
-                            .enableBaseResultMap() // 启用 BaseColumnList    默认值:false
-                            .enableBaseColumnList()
+                            .mapperBuilder()                // mapper 策略配置
+                            .superClass(BaseMapper.class)   // 设置父类
+                            .enableMapperAnnotation()       // 开启 @Mapper 注解    默认值:false
+                            .enableBaseResultMap()          // 启用 BaseResultMap 生成    默认值:false
+                            .enableBaseColumnList()         // 启用 BaseColumnList    默认值:false
                     ;
 
                 })
@@ -45,6 +49,4 @@ public class Generator {
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
     }
-
-
 }
